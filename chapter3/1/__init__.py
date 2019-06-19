964fe6b3d5870fc2150bd4c173389966b4792739
wang yingxin <1196914202@qq.com>
2019-06-19 22:10:58 +0800

    justify directory

--- chapter3/1/__init__.py
+++ chapter3/1/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# @Time    : 2019/6/19 20:23
+# @Time    : 2019/6/19 22:07
 # @Author  : Wang
 # @FileName: __init__.py.py
 # @Software: PyCharm
--- chapter3/1/test_urlopen.py
+++ chapter3/1/test_urlopen.py
@@ -5,21 +5,22 @@ import urllib.error
 
 response = urllib.request.urlopen('https://www.python.org')
 # print(response.read().decode('utf-8'))  #输出网页源码
-print(type(response))
-print(response.status)
-print(response.getheaders())
-print(response.getheader('Server'))
+print('type of response: ', type(response))
+print('status of response: ', response.status)
+print('getheaders method: ', response.getheaders())
+print('getheaders method containing parameters: ', response.getheader('Server'))
 
 # 测试data参数
 data = bytes(urllib.parse.urlencode({'word': 'hello'}), encoding='utf-8')
 response = urllib.request.urlopen('http://httpbin.org/post', data=data)
-print(response.read())
+print('read method: ', response.read())
 
 # # 测试timeout参数, 单位为秒
 # response = urllib.request.urlopen('http://httpbin.org/get', timeout=0.1)
 # print(response.read())
 
 # 测试error
+print('--------test error---------')
 try:
     response = urllib.request.urlopen('http://httpbin.org/get', timeout=0.1)
 except urllib.error.URLError as e:
